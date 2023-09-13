8ec8be3f66fc433546152b1d08efb7b7e3bb51bc
Sigit Wahyudi <sigitwahyu.mutu@gmail.com>
2023-09-13 22:01:25 +0700

    feat: showing apikey

--- assignment-2/currency-rates-app/src/App.js
+++ assignment-2/currency-rates-app/src/App.js
@@ -3,7 +3,8 @@ import React from "react";
 import { useFetch } from "./hooks/use-fetch";
 
 const App = () => {
-	const apikey = process.env.REACT_APP_APIKEY;
+	const apikey =
+		process.env.REACT_APP_CURRENCYAPIKEY || "7007dd9760884cbb93cb98e4775f451f";
 	const sysmbols = "CAD,EUR,IDR,JPY,CHF,GPB";
 	const baseUrl = "https://api.currencyfreaks.com";
 	const url = `${baseUrl}/v2.0/rates/latest?apikey=${apikey}&symbols=${sysmbols}`;
@@ -25,17 +26,17 @@ const App = () => {
 	const tableHeader = ["Currency", "We Buy", "Exchange Rate", "We Sell"];
 
 	if (isLoading) {
-		return <div className='app-container'>Loading</div>;
+		return <div className="app-container">Loading</div>;
 	}
 
 	if (isError && !isLoading) {
-		return <div className='app-container'>{error}</div>;
+		return <div className="app-container">{error}</div>;
 	}
 
 	return (
-		<div className='app-container'>
+		<div className="app-container">
 			<table>
-				<thead className='table-header'>
+				<thead className="table-header">
 					<tr>
 						{tableHeader.map((item) => (
 							<td key={item}>{item}</td>
@@ -57,7 +58,7 @@ const App = () => {
 			<p>Rates are based from 1 {data.base}</p>
 			<p>
 				This application uses API from{" "}
-				<a target='_blank' rel='noreferrer' href={baseUrl}>
+				<a target="_blank" rel="noreferrer" href={baseUrl}>
 					{baseUrl}
 				</a>
 			</p>
